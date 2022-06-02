3631682b4c8b0652175c3d173a40958c472aefed
lucasfreittas <l.freittas@outlook.com>
2022-06-02 17:45:46 -0300

    Route and Controller create user

--- src/server.js
+++ src/server.js
@@ -2,13 +2,30 @@
 
 const express = require('express');
 const sqliteConnection = require('./database/knex')
+const routes = require('./routes')
 const app = express();
 app.use(express.json());
 
 
 //-----------------------------INIT------------------------//
 sqliteConnection()
+app.use(routes);
 
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        
+    return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message
+    });
+}
+    console.error(error.message);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+});
 
 
 
